66844a56be5a2b0d6c9cbd26e80e028119a65378
xuquanhua <350339018@qq.com>
2016-12-01 10:53:08 +0800

    12-01

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# gogo
+#1. 安装最新版本node (最新版已经包含npm)
+#2. npm install --save-dev webpack
+#2. npm install --save-dev gulp
+#3. npm install
+#5. gulp test --uglify
--- gulpfile.js
+++ gulpfile.js
@@ -1,111 +1,133 @@
-﻿/**
+/**
  * 组件安装
- * npm install gulp-imagemin gulp-sass gulp-replace gulp-minify-css gulp-rename gulp-concat gulp-clean gulp-livereload gulp-plumber --save-dev
  */
 
-var gulp = require('gulp'),
-    imagemin = require('gulp-imagemin'),
-    sass = require('gulp-sass'),
-    rename = require('gulp-rename'),
-    clean = require('gulp-clean'),
-    plumber = require('gulp-plumber'),
-    uglify = require('gulp-uglify'),
-    concat = require('gulp-concat'),
+// 引入 gulp及组件
+var gulp = require('gulp'), //基础库
+    rename = require('gulp-rename'), //重命名
+    concat = require('gulp-concat'), //合并文件
+    clean = require('gulp-clean'), //清空文件夹
+    replace = require('gulp-replace'), //字符串替换
+    gutil = require('gulp-util'), //抛错误
     minifycss = require('gulp-minify-css'),
-    sass = require('gulp-sass'),
-    sourcemaps = require('gulp-sourcemaps'),
-    replace = require('gulp-replace'),
-    browserSync = require('browser-sync');
+    uglify = require('gulp-uglify'), //js压缩
+    gulpSequence = require('gulp-sequence'),
+    gulpif = require('gulp-if'),
+    cheerio = require('gulp-cheerio'),
+    run = require('gulp-run'),
+    webpack = require('webpack'),
+    webpackConfig = require('./webpack.config.js'),
+    md5 = require('gulp-md5-plus');
 
-gulp.task('s', function() {
-    browserSync({
-        server: {
-            baseDir: "./dist"
-        }
-    });
-    gulp.watch('.src//sass/*.scss', ['sass']);
-    gulp.watch("./src/**/*.html", ['html']);
-    gulp.watch("./src/**/*.js", ['js']);
-    gulp.watch("./dist/**/*.html").on('change', browserSync.reload);
-});
-
-
+var srcDst = './src/';
+var relDst = './dist/';
+var nestedSrc = function() {
+    var temp = false;
+    if (process.argv.indexOf('--uglify') !== -1) {
+        temp = true;
+    }
+    return temp
+};
+// HTML处理
 gulp.task('html', function() {
-    var htmlSrc = ['./src/*.html'],
-    htmlDst = './dist';
-    return gulp.src(htmlSrc)
-        .pipe(gulp.dest(htmlDst));
+    var Src = [srcDst + './**/*.html'],
+        Dst = relDst + './';
+    return gulp.src(Src)
+        .pipe(cheerio({
+            run: function($) {
+                var replace = $('script[compress]').remove();
+                var tempArray = [];
+                var tempStr;
+                for (var i = 0, length = replace.length; i < length; i++) {
+                    tempStr = $(replace[i]).attr('compress');
+                    console.log(tempStr);
+                    if (tempArray.indexOf(tempStr) === -1) {
+                        $('body').append('<script src="' + tempStr + '"></script>');
+                        tempArray.push(tempStr);
+                    }
+                }
+            },
+            parserOptions: {
+                decodeEntities: false
+            }
+        }))
+        .pipe(gulp.dest(Dst));
 });
 
-gulp.task('lib_css', function() {
-    var libSrc = './src/lib/*.css',
-        libDst = './dist/lib';
-    return gulp.src(libSrc)
-                .pipe(rename({suffix: '.min'}))
-                .pipe(minifycss())
-                .pipe(gulp.dest(libDst));
-});
-gulp.task('lib_js', function() {
-    var libSrc = './src/lib/*.js',
-        libDst = './dist/lib';
-    return gulp.src(libSrc)
-                .pipe(rename({suffix: '.min'}))
-                .pipe(uglify())
-                .pipe(gulp.dest(libDst));
+//目前只对单个css压缩
+gulp.task('css', function() {
+    var Src = srcDst + 'css/**/*.*',
+        Dst = relDst + 'css/';
+    return gulp.src(Src)
+        .pipe(gulpif(nestedSrc, minifycss()))
+        .pipe(gulp.dest(Dst));
 });
 
-gulp.task('sass', function () {
-    return gulp.src('./src/scss/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./src/css'));
-});
-
-gulp.task('css', ['sass'], function() {
-    return gulp.src('./src/css/*.css')
-        .pipe(concat('main.css'))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(minifycss())
-        .pipe(gulp.dest('./dist/css/'));
-});
-
-gulp.task('replaceCssSrc', ['html'], function () {
-    var dateString = (new Date()).getTime()+'">';
-    return gulp.src(['./dist/*.html'])
-                .pipe(replace(/\.css.*/g, '.min.css?v=' + dateString ))
-                .pipe(gulp.dest('./dist'));
-});
-
-gulp.task('replaceJsSrc', ['replaceCssSrc'], function () {
-    var dateString = new Date().getTime()+ '"></script>';
-    return gulp.src(['./dist/*.html'])
-                .pipe(replace(/\.js.*/g, '.min.js?v=' + dateString ))
-                .pipe(gulp.dest('./dist'));
-});
-
-gulp.task('js', function() {
-    var jsSrc = ['./src/lib/zepto.js','./src/lib/sm.js','./src/js/Common.js'],
-        jsDst = './dist/js';
-    return gulp.src(jsSrc)
+//js concat index
+gulp.task('index_js', function() {
+    var Src = ['./src/js/index/a.js', './src/js/index/b.js'],
+        Dst = relDst + 'js/index/';
+    return gulp.src(Src)
         .pipe(concat('all.js'))
-        .pipe(gulp.dest("./src/js"))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(uglify())
-        .pipe(gulp.dest(jsDst));
-})
+        .pipe(gulpif(nestedSrc, uglify()))
+        .pipe(gulp.dest(Dst));
+});
 
+//目前只对单个js压缩(少了lib)
+gulp.task('js', function() {
+    var Src = srcDst + 'js/*.*',
+        Dst = relDst + 'js/';
+    return gulp.src(Src)
+        .pipe(gulpif(nestedSrc, uglify()))
+        .pipe(gulp.dest(Dst));
+});
+
+//复制images
 gulp.task('images', function() {
-    var imgSrc = './src/images/*',
-        imgDst = './dist/images';
-    return gulp.src(imgSrc)
-                .pipe(imagemin())
-                .pipe(gulp.dest(imgDst));
-})
+    var Src = srcDst + 'images/**/*.*',
+        Dst = relDst + 'images/';
+    return gulp.src(Src)
+        .pipe(gulp.dest(Dst));
+});
 
+//给js加md5并插入hmtl里
+gulp.task('md5:js', function(done) {
+    gulp.src([relDst + 'js/**/*.js', '!' + relDst + 'js/log/**/*.js'])
+        .pipe(md5(10, relDst + './**/*.html'))
+        .pipe(gulp.dest(relDst+ 'js/'))
+        .on('end', done);
+});
+
+//给css加md5并插入hmtl里
+gulp.task('md5:css', function(done) {
+    gulp.src(relDst + 'css/**/*.css')
+        .pipe(md5(10, relDst + './**/*.html'))
+        .pipe(gulp.dest(relDst+ 'css/'))
+        .on('end', done);
+});
+
+// 清空图片、样式、js
 gulp.task('clean', function() {
-    return gulp.src(['./dist/css', './dist/js', './dist/images','./dist/lib'], { read: false })
-                .pipe(clean());
+    return gulp.src([relDst], { read: false })
+        .pipe(clean({ force: true }));
 });
 
-gulp.task('default', ['clean'], function() {
-    return gulp.start('images', 'sass', 'js', 'lib_css', 'lib_js', 'css', 'html', 'replaceCssSrc','replaceJsSrc');
+var myDevConfig = Object.create(webpackConfig);
+var devCompiler = webpack(myDevConfig);
+
+//引用webpack对js进行操作
+gulp.task("build-js",  function(callback) {
+    devCompiler.run(function(err, stats) {
+        if(err) throw new gutil.PluginError("webpack:build-js", err);
+        gutil.log("[webpack:build-js]", stats.toString({
+            colors: true
+        }));
+        callback();
+    });
 });
+
+
+
+
+// gulp test --uglify
+gulp.task('test', gulpSequence("clean", 'css','images', 'js', "build-js", 'html', 'md5:css', 'md5:js'));
